69c11102c55a2b4213082c71b6d5f6bb5b088823
monarene <mekuleyimichael@gmail.com>
2020-12-01 07:44:08 +0100

    decode

--- bin/dart_intermediate.dart
+++ bin/dart_intermediate.dart
@@ -1,6 +1,19 @@
 import 'package:dart_intermediate/dart_intermediate.dart' as dart_intermediate;
+import 'dart:convert';
 
 void main(List<String> arguments) {
-  dart_intermediate.sayHello();
+  // dart_intermediate.sayHello();
   print('Hello world: ${dart_intermediate.calculate()}!');
+
+// To encode string to bas64
+  String myValue = 'Hello World';
+  List ebytes = utf8.encode(myValue);
+  String encoded = base64.encode(ebytes);
+  // print(ebytes);
+  print(encoded);
+
+  //To decode string from base64
+  List dbytes = base64.decode(encoded);
+  String decoded = utf8.decode(dbytes);
+  print(decoded);
 }
